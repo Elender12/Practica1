fe1b502c4513d78956af105776e3710db3ad06d8
Ser1441 <53231703+Ser1441@users.noreply.github.com>
2019-10-17 18:28:53 +0200

    holi
    goli

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<body>
+    <div>
+    <h1>VARIABLES Y ARRAYS</h1>
+
+
+    </div>
+    <div>
+
+    </div>
+
+
+</body>
+</html>
