8d6498a9ace94eba4bce0f7ec25fb30154caea8b
Ser1441 <53231703+Ser1441@users.noreply.github.com>
2019-10-17 19:18:43 +0200

    subio
    subio

---
+++ jugar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jugar</title>
+</head>
+<body>
+    <div>
+    <form action="jugador.php">
+    Piedra<input type="radio" name="eleccion" value="piedra"/>
+    Papel<input type="radio" name="eleccion" value="papel"/>
+    Tijeras<input type="radio" name="eleccion" value="tijeras"/>
+    Lagarto<input type="radio" name="eleccion" value="lagarto"/>
+    Spok<input type="radio" name="spok" value="spok"/></p>
+
+    <p><input type="submit" value="Jugar"/></p>
+    </div>
+    <div>
+    </form>
+    <form action="index.php">
+    <input type="submit" value="Salir"/>
+    </form>
+    <div>
+</body>
+</html>
+
