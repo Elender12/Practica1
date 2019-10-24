4cc239f2660612a24c86650a82bc2e508e0c1f43
ElenDeR <43650610+Elender12@users.noreply.github.com>
2019-10-24 17:07:04 +0200

    Función del buCle
    Debería funcionar...Warning

--- jugador.php
+++ jugador.php
@@ -2,23 +2,25 @@
 session_start();
 
 // Inclusion de clases y ficheros
-require ("jugar.php");
-require ("clases/controladorJuego.php");
+include_once ("jugar.php");
+require ("controladorJuego.php");
 
-// Asignamos a la variable de sesion el dinero inical para que
+// Asignamos a la variable de sesion el dinero inicial para que
 // este diponible durante toda la ejecucion del programa
 if(isset($_POST['money'])){
     $money = $_POST['money'];
     $_SESSION['money'] = $money;
+   
 }
 if(isset($_POST['nombre'])){
-    $nombre = $_POST['nombre'];
-    $_SESSION['nombre'] = $nombre;
+	$nombre= $_POST['nombre'];
+	$_SESSION['nombre'] = $nombre;
+
 }
 
 // Si hemos elegido una tirada comenzará el programa
 if(isset($_POST['eleccion'])){
-    comenzarJuego();  
+    comenzarJuego();
 }
 
 // Funcion que contiene la logica del juego
@@ -40,11 +42,15 @@ function comenzarJuego(){
     
     // Creamos una instancia de la clase que contiene la logica en caso de ganar, perder o empatar
     $controladorJuego = new controladorJuego;
-        
-    $controladorJuego->comprobarTirada($eleccionOponente, $eleccionJugador, $resultado);
 
+    //if($resultado == 1){         
+    $controladorJuego->comprobarTirada($eleccionOponente, $eleccionJugador, $resultado);
+    //}elseif ($resultado == -1) {
+    //    $controladorJuego->perder($eleccionOponente, $eleccionJugador);
+    //}else {
+    //    $controladorJuego->empatar($eleccionOponente, $eleccionJugador);
+    //}
     mostrarHistorial($eleccionOponente, $eleccionJugador);
-    
 }
 
 // Genera la eleccion del oponente
@@ -58,7 +64,7 @@ function generarTiradaMaquina($numAleatorio){
 
 // Muestra el historial de los ultimos movimientos
 function mostrarHistorial($eleccionOponente, $eleccionJugador){
-    $movimientosAMostrar = 5;
+    $movimientosAMostrar = 0;
     if( !isset($_SESSION['tu']) && !isset($_SESSION['machine'])){
         $tu= array();
         $maquina=array();
@@ -70,22 +76,24 @@ function mostrarHistorial($eleccionOponente, $eleccionJugador){
     array_unshift($_SESSION['tu'],$eleccionJugador);
     array_unshift($_SESSION['machine'],$eleccionOponente);
 
-
     if(count($_SESSION['tu'])<$movimientosAMostrar){
         $movimientosAMostrar=count($_SESSION['tu']);
     }
     echo"<table>";
     echo "<tr>";
     echo "<td>".$_SESSION['nombre']."</td>";
-    for ($i=0; $i < $movimientosAMostrar; $i++) { 
-            echo "<td>".$_SESSION['tu'][$i]."</td>"; 
-    }
-    echo"</tr>";
+    bucle($_SESSION['tu'],$movimientosAMostrar);
     echo "<tr>";
     echo "<td> Makina </td>";
-    for ($i=0; $i < $movimientosAMostrar; $i++) { 
-        echo "<td>".$_SESSION['machine'][$i]."</td>";
-    }
+    bucle($_SESSION['machine'],$movimientosAMostrar);
     echo"</table>";
 }
+    //funcion para reutilizar el for para generar la tabla de ultimas tiradas
+    // la variable v es la variable de session a recorrer
+    function bucle($v,$movimientosAMostrar){
+        for ($i=0; $i < $movimientosAMostrar; $i++) { 
+                echo "<td>".$v[$i]."</td>"; 
+        }
+        echo"</tr>";
+    }
 ?>
